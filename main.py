82d63b91ed5ccd189ac1fa1d540907c9d5e5877c
sujaymondal0038-source <sujaymondal0038@gmail.com>
2026-02-03 13:26:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 import uvicorn
 
@@ -8,4 +9,5 @@ def root():
     return {"status": "Buddy backend running"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)

8328046d80b00e0e8960a3ab6be41674ebaea947
sujaymondal0038-source <sujaymondal0038@gmail.com>
2026-02-03 16:43:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
-import os
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "Buddy backend running"}
+
+@app.get("/health")
+def health():
+    return {"health": "ok"}
+
+@app.get("/hello/{name}")
+def hello(name: str):
+    return {"msg": f"Hello {name}"}
